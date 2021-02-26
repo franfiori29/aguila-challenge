0e4cd882174cca303c7e958e2057ebc8f51bde05
franfiori29 <franfiori@gmail.com>
2021-02-26 13:58:24 -0300

    icon components, styles and routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
 import './App.css';
+import { useEffect } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './components/Home';
+import { AppProvider } from './context';
+import Todos from './components/Todos';
+import Users from './components/Users';
+import Sidebar from './components/Sidebar';
+import Navbar from './components/Navbar';
+const { REACT_APP_API } = process.env;
 
 function App() {
-	return <div></div>;
+	useEffect(() => {
+		fetch(`${REACT_APP_API}/users`)
+			.then((res) => res.json())
+			.then((users) =>
+				console.log(
+					'users',
+					users.message.sort((a, b) => (a.firstName > b.firstName ? 1 : -1))
+				)
+			);
+	}, []);
+
+	return (
+		<AppProvider>
+			<BrowserRouter>
+				<Sidebar />
+				<Navbar />
+				<Route exact path='/'>
+					<Home />
+				</Route>
+				<Route exact path='/usuarios'>
+					<Users />
+				</Route>
+				<Route exact path='/tareas'>
+					<Todos />
+				</Route>
+			</BrowserRouter>
+		</AppProvider>
+	);
 }
 
 export default App;
